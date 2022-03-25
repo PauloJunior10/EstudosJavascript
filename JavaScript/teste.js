a47e27c093fa8984da2402b74fc6a0f6a0ba9667
Paulo <paulojunior.10@outlook.com>
2022-03-24 22:48:40 -0300

    atualizoes

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,12 +1,13 @@
-// Para cada Elemento:
-// Retorne uma string com o nome da pessoa
-// Remova apenas a chave nome do objeto
-// Adicione uma chave ID em cada objeto
-const pessoas = [
-    {nome: 'joao',      idade: 52},
-    {nome: 'maria',     idade: 34},
-    {nome: 'alberto',   idade: 23},
-    {nome: 'lais',      idade: 45},
-    ]
-    
-    
+// As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia e R$ 1,00 se forem compradas pelo menos 12.
+
+// Escreva uma função receba o número de maçãs compradas, calcule e retorne o custo total da compra
+
+function calculaPrecoTotal(quantidade) {
+    // Escreva seu código aqui
+    let valor = 0
+    quantidade <= 12 ? valor = 1.30 : valor = 1
+
+    return quantidade*valor
+  }
+
+  console.log(calculaPrecoTotal(5))

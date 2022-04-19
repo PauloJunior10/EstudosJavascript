06e26b878382c02e4380701b88fdb9b9af05c3d2
Paulo <paulojunior.10@outlook.com>
2022-04-19 18:39:17 -0300

    excluindo algumas pastas

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,27 +1,12 @@
-function filtraTarefas() {
-    const tarefas = [
-      { titulo: "Fazer Remember", status: "done", tempo: 30 },
-      { titulo: "Fazer Challange", status: "todo", tempo: 30 },
-      { titulo: "Assistir Aula", status: "done", tempo: 120 },
-      { titulo: "Fazer almoço nutritivo", status: "done", tempo: 60 },
-      { titulo: "Ler atentamente exercícios do dia", status: "doing", tempo: 20 },
-      { titulo: "Fazer exercício do dia", status: "doing", tempo: 180 },
-      { titulo: "Fazer desafio", status: "doing", tempo: 60 },
-      { titulo: "Ir para o Stand Up", status: "todo", tempo: 30 },
-      { titulo: "Enviar Feedback", status: "todo", tempo: 15 },
-      { titulo: "Enviar Exercício", status: "todo", tempo: 15 },
-      { titulo: "Jogar Videogame", status: "todo", tempo: 120 },
-      { titulo: "Assistir Novela", status: "todo", tempo: 90 }
-   ]
-  
-   // Escreva seu código aqui
-  let novoArray = []
-  for(let i =0; i<tarefas.length; i++){
-      if(tarefas[i].status === "done"){
-          novoArray.push(tarefas[i].titulo)
+function palindrome(str) {
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) {
+          return false;
       }
-  }
-  return novoArray
-}
-
-console.log(filtraTarefas())
+    }
+    return true;
+   }
+   console.log(palindrome("arara"))

4b7353514955c001833e58e378dbc3bf038d3950
Paulo <paulojunior.10@outlook.com>
2022-05-03 22:36:20 -0300

    this

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,11 +1,10 @@
-// As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12. Faça um programa que leia o número de maçãs compradas, calcule e imprima o custo total da compra.
-
-function valorTotal(quantidade){
-    let maca = 1.3 
-    if(quantidade > 11){
-        maca = 1
+const pessoa = {
+    primeiroNome: "Paulo",
+    sobreNome: "Junior",
+    id: 1,
+    nomeCompleto: function (){
+        return this.primeiroNome+ " "+this.sobreNome
     }
-    return quantidade * maca
-}
+} 
 
-console.log(valorTotal(12))
+console.log(pessoa.nomeCompleto())

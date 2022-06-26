4187ad26ca663a7163937cf771c3cffaafd3814d
PauloJunior <paulojunior.10@outlook.com>
2022-06-26 10:38:00 -0300

    desestruturacao

--- JavaScript/Objects/Desestruturação.js
+++ JavaScript/Objects/Desestruturação.js
@@ -1,9 +1,45 @@
-const pessoa = {
-    nome1: "paulo",
-    nome2: "junior"
-}
+// const pessoa = {
+//     nome1: "paulo",
+//     nome2: "junior"
+// }
 
-const {nome1, nome2} = pessoa
+// const {nome1, nome2} = pessoa
 
-console.log(nome1)
+// console.log(nome1)
 
+// =================
+
+// const retornaObjeto = () => {
+//     const person = {
+//       name: 'Marina',
+//       age: 32,
+//       profession: 'teacher'
+//     }
+  
+//     return person
+//   }
+//   const result1 = retornaObjeto()
+// console.log(result1)// retornou todo o objeto
+
+// const { name, age, profession } = result1
+// console.log(name)
+// console.log(age)
+// console.log(profession)
+// ======================
+
+// EXEMPLO COM ARRAY DE OBJETOS
+const petsDaMazzi = [
+    { nome: 'Laura', tipo: 'cachorra' },
+    { nome: 'Leia', tipo: 'cachorra-boi' },
+    { nome: 'Luke', tipo: 'cachorro-líquido' },
+    { nome: 'Buda', tipo: 'gatita' },
+    { nome: 'Baleia', tipo: 'gato-rebelde' }
+  ]
+  
+  const [laura, leia, luke, buda, baleia] = petsDaMazzi
+  
+  console.log(laura)
+  console.log(leia)
+  console.log(luke)
+  console.log(buda)
+  console.log(baleia)

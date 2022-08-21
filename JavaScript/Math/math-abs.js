146d3801fd42fcfcdc5da3b8ceb5bd676e5b6b64
PauloJunior <paulojunior.10@outlook.com>
2022-08-21 18:03:10 -0300

    exercicio de foreach emath.abs2

---
+++ JavaScript/Math/math-abs.js
@@ -0,0 +1,9 @@
+// O método Math.abs() retorna o valor absoluto de um número.
+
+let a = Math.abs(7.25);
+let b = Math.abs(-7.25);
+let c = Math.abs(null);
+let d = Math.abs("Hello");
+let e = Math.abs(2-3);
+
+console.log(a,b,c,d,e)
--- JavaScript/Math/max-min.js
+++ JavaScript/Math/max-min.js
@@ -1,11 +1,14 @@
+//  o método Math.min() retorna o menor valor
+//  o método Math.max() retorna o maior valor
+
 let a = Math.min(5, 10);
-let b = Math.min(0, 150, 30, 20, 38);
+let b = Math.min(16, 150, 30, 20, 38);
 let c = Math.min(-5, 10);
 let d = Math.min(-5, -10); // ele entende que -10 é menor que -5
 let e = Math.min(1.5, 2.5);
 
 let f = Math.max(5, 10);
-let g = Math.max(0, 150, 30, 20, 38);
+let g = Math.max(0, 50, 30, 20, 38);
 let h = Math.max(-5, 10);
 let i = Math.max(-5, -10);
 

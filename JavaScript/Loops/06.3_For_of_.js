2a3d484bc6d13b2d04c16a23c09fb6f272a0a6ec
PauloJunior <paulojunior.10@outlook.com>
2022-08-28 11:32:00 -0300

    aprofundando em for of

--- JavaScript/Loops/06.3_For_of_.js
+++ JavaScript/Loops/06.3_For_of_.js
@@ -2,25 +2,45 @@
 // Ele permite que você faça um loop sobre estruturas de dados iteráveis, como Arrays, Strings, Maps, NodeLists e muito mais:
 
 // AQUI RECEBE UMA STRING E PERCORRE ELA
-//a)
-// let language = "JavaScript";
+// abaixo forma simples
+let linguagem = 'PYTHON'
+for(let i of linguagem){
+    console.log(i)
+}
 
-// let text = "";
+//a) forma mais complexa
+let language = "CODE";
 
- //  variavel index recebe 'de'/'of' language
-// for (let index of language) {
-//   text += index + '\n';
+let text = "";
+
+//   variavel index recebe 'de/of' language
+for (let index of language) {
+  text += index + '\n';
   
-// }
-// console.log(text)
+}
+console.log(text)
 
 
-//b)
- const arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+//b) com Arrays
+//  const arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
  
- // variavel item recebe 'do'/'of' arrayoriginal
-    for (item of arrayOriginal) {
-        console.log(item)
+//  // variavel item recebe 'do'/'of' arrayoriginal
+//     for (item of arrayOriginal) {
+//         console.log(item)
         
-    }
+//     }
+
+// c) com map
+// define Map
+let map = new Map();
+
+// inserting elements
+map.set('name', 'Jack');
+map.set('age', '27');
+
+console.log(map)
+// looping through Map
+for (let [key, value] of map) {
+    console.log(key + '- ' + value);
+}
         

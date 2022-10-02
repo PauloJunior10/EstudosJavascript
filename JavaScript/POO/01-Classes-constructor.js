ba064da41ac831c4fa079a9923558c239fe0effe
PauloJunior <paulojunior.10@outlook.com>
2022-10-02 09:04:43 -0300

    criando a pasta de POO

---
+++ JavaScript/POO/01-Classes-constructor.js
@@ -0,0 +1,7 @@
+function Cachorro(raca) { // por convenção  inicamos classes com letras maiusculas
+    this.raca = raca // criamos uma propriedade raca e setamos ela
+}
+
+let husky = new Cachorro("Husky")
+
+console.log(husky.raca)

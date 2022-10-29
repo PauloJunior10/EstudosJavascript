74c3e7887d57cc68624e4132fb927a59b1e115f7
PauloJunior <paulojunior.10@outlook.com>
2022-10-29 20:45:41 -0300

    exercicios do livro eloquent javascript

---
+++ JavaScript/TestesEmpregos/EloquentJavascript/01.LoopingATriangle.js
@@ -0,0 +1,12 @@
+// Escreva um loop que faça sete chamadas para console.logpara gerar o seguinte triângulo:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);

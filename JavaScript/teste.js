dfb4437a65942e01fb74e40cfd79e166c1e73c0b
PauloJunior <paulojunior.10@outlook.com>
2023-10-29 21:53:59 -0300

    continuando os estudos de javascript

---
+++ JavaScript/teste.js
@@ -0,0 +1,12 @@
+const arr2 = ["Stimpson", "PAUL", "JAVCS", "TOM", "J", "cat"];
+
+// Defina o novo elemento que você deseja inserir
+// const novoElemento = "JUNIN";
+
+// Substitui o terceiro elemento pelo novo elemento
+arr2[2] = "novoElemento";
+
+console.log(arr2); // O array agora terá o novo elemento no lugar do terceiro elemento
+
+
+

f550704c621259c8d37cb60e4e779e2884ea7a84
PauloJunior <paulojunior.10@outlook.com>
2024-06-26 22:36:13 -0300

    exclui a pasta DOM

--- JavaScript/DOM/04-manipulando-atributos/script.js
+++
@@ -1,9 +0,0 @@
-function trocarImagem (filename, animalname) {
-    document.querySelector('.imagem').setAttribute('src','imagens/' +filename);
-    document.querySelector('.imagem').setAttribute('data-animal', animalname);
-}
-
-function pegarAnimal(){
-    let animal = document.querySelector('.imagem').getAttribute('data-animal');
-    alert('O animal é: ' + animal)
-}
